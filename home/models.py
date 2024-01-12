e1171faf8d6062f5e44987d7717301466509f07b
M1rshad <abdullamirshadcl@gmail.com>
2024-01-13 02:15:45 +0530

    Add Product Model

--- home/models.py
+++ home/models.py
@@ -10,3 +10,20 @@ class Category(models.Model):
 
     def __str__(self) -> str:
         return self.category_name
+    
+
+class Product(models.Model):
+    product_name = models.CharField(max_length=100, unique=True)
+    slug = models.SlugField(max_length=100, unique=True)
+    description = models.TextField(max_length=255, blank=True)
+    price = models.IntegerField()
+    product_image = models.ImageField(upload_to='images/products')
+    stock = models.IntegerField()
+    is_available = models.BooleanField(default=True)
+    category = models.ForeignKey(Category, on_delete = models.CASCADE)
+    created_date = models.DateTimeField(auto_now_add=True)
+    modified_date = models.DateTimeField(auto_now=True)
+
+
+    def __str__(self) -> str:
+        return self.product_name
