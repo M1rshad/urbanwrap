092672de3440c6a445ea484a11123ec1425a8b70
M1rshad <abdullamirshadcl@gmail.com>
2024-01-18 17:18:05 +0530

    First commit

--- user_auth/views.py
+++ user_auth/views.py
@@ -13,19 +13,29 @@ def sign_up(request):
         form = SignupForm(request.POST)
         if form.is_valid():
             form.save()
+            messages.success(request, 'Your account is created')
             return redirect(log_in)
     context = {'form' : form}
     return render(request, 'user_auth/user_signup.html', context)
 
 def log_in(request):
-    email = request.POST.get('email')
-    password = request.POST.get('password')
-    user = authenticate(email=email, password=password)
-    if user is not None and user.is_active:
-        login(request, user)
-        return redirect(index)
-    else:
-        messages.error(request, 'Invalid username or password')
+    if request.POST:
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+        user = authenticate(request, email=email, password=password)
+        if user is not None:
+            print(f"user.is_active: {user.is_active}")
+            if user.is_active:
+                login(request, user)
+                return redirect(index)
+            elif not user.is_active:
+                messages.error(request, 'Your account is blocked. Please contact support.')
+            else:
+                messages.error(request, 'Invalid username or password')
+        else:
+            messages.error(request, 'Invalid username or password')
+        
+
     return render(request, 'user_auth/user_login.html')
 
 def log_out(request):
