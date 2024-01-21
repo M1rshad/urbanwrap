04bbc59f4fc41fe741aa5a4cea1bb53810063f94
M1rshad <abdullamirshadcl@gmail.com>
2024-01-21 15:01:04 +0530

    Update cart

--- shop/urls.py
+++ shop/urls.py
@@ -6,6 +6,9 @@ urlpatterns = [
     path('shop/<slug:product_slug>', views.product_detail, name='product_detail'),
     path('cart/', views.cart, name='cart'),
     path('add-cart/<int:product_id>/', views.add_cart, name='add_cart'),
+    path('remove-cart-item/<int:product_id>/', views.remove_cart_item, name='remove_cart_item'),
+    path('decrement-cart-item/<int:product_id>/', views.decrement_cart, name='decrement_cart'),
+
 
 
 ]
--- shop/views.py
+++ shop/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect, HttpResponse
+from django.shortcuts import render, redirect, get_object_or_404
 from home.models import Product
 from .models import Cart, CartItem
 
@@ -40,19 +40,35 @@ def add_cart(request, product_id):
         )
         cart.save()
     try:
-        cart_item = CartItem.objects.get(products=product, cart=cart)
+        cart_item = CartItem.objects.get(product=product, cart=cart)
         cart_item.quantity += 1
         cart_item.save()
     except CartItem.DoesNotExist:
         cart_item = CartItem.objects.create(
-            products = product,
+            product = product,
             quantity = 1,
             cart = cart
         )
         cart_item.save()
     return redirect('cart')
 
+def decrement_cart(request, product_id):
+    cart = Cart.objects.get(cart_id=_cart_id(request))
+    product = get_object_or_404(Product, id=product_id)
+    cart_item = CartItem.objects.get(cart=cart, product=product)
+    if cart_item.quantity > 1 :
+        cart_item.quantity -= 1
+        cart_item.save()
+    else:
+        cart_item.delete()
+    return redirect('cart')
 
+def remove_cart_item(request, product_id):
+    cart = Cart.objects.get(cart_id=_cart_id(request))
+    product = get_object_or_404(Product, id=product_id)
+    cart_item = CartItem.objects.get(cart=cart, product=product)
+    cart_item.delete()
+    return redirect('cart')
 
 
 def cart(request, total=0, quantity=0, cart_items=None):
@@ -62,11 +78,16 @@ def cart(request, total=0, quantity=0, cart_items=None):
         for cart_item in cart_items:
             total += (cart_item.product.price * cart_item.quantity)
             quantity += cart_item.quantity
+        tax =  0.2 * total
+        grand_total = total + tax
     except Cart.DoesNotExist:
         pass
     context = {
         'total':total,
         'quantity':quantity,
-        'cart_items' : cart_items
+        'cart_items' : cart_items,
+        'tax' : tax,
+        'grand_total':grand_total,
     }
     return render(request, 'shop/cart.html', context)
+
