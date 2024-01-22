c852144e0fd75c700b07398e58dc57abcccdaf99
M1rshad <abdullamirshadcl@gmail.com>
2024-01-22 22:53:46 +0530

    Update cart and variation Update variation model Fix template

--- shop/urls.py
+++ shop/urls.py
@@ -7,9 +7,7 @@ urlpatterns = [
     path('shop/category/<slug:category_slug>/<slug:product_slug>', views.product_detail, name='product_detail'),
     path('cart/', views.cart, name='cart'),
     path('add-cart/<int:product_id>/', views.add_cart, name='add_cart'),
-    path('remove-cart-item/<int:product_id>/', views.remove_cart_item, name='remove_cart_item'),
-    path('decrement-cart-item/<int:product_id>/', views.decrement_cart, name='decrement_cart'),
+    path('remove-cart-item/<int:product_id>/<int:cart_item_id>', views.remove_cart_item, name='remove_cart_item'),
+    path('decrement-cart-item/<int:product_id>/<int:cart_item_id>', views.decrement_cart, name='decrement_cart'),
     path('shop/search/', views.search, name='search'),
-
-
 ]
