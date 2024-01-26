360234d44c79a483c1aaf39b33a1aa725d4c2101
M1rshad <abdullamirshadcl@gmail.com>
2024-01-27 03:35:05 +0530

    Update home page

--- home/views.py
+++ home/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
-
+from .models import Product
 # Create your views here.
 def index(request):
-    return render(request, 'home/index.html')
+    featured = Product.objects.all().order_by('-priority','id')[:4] 
+    context = {
+        'featured':featured,
+    }
+    return render(request, 'home/index.html', context)
 
 def dashboard(request):
     return render(request, 'home/dashboard.html')
--- shop/views.py
+++ shop/views.py
@@ -12,13 +12,13 @@ def shop(request, category_slug=None):
     if category_slug != None:
         categories = get_object_or_404(Category, slug=category_slug, is_active=True)
         products = Product.objects.filter(category=categories, is_available=True)
-        paginator = Paginator(products, 4)
+        paginator = Paginator(products, 12)
         page = request.GET.get('page')
         paged_product = paginator.get_page(page)
         product_count = products.count()
     else:
         products = Product.objects.all().filter(is_available=True).order_by('id')
-        paginator = Paginator(products, 4)
+        paginator = Paginator(products, 12)
         page = request.GET.get('page')
         paged_product = paginator.get_page(page)
         product_count = products.count()
