90dd639da08f6c02342028c758083e649c757124
M1rshad <abdullamirshadcl@gmail.com>
2024-02-04 20:48:52 +0530

    Add User profile model

--- user_auth/models.py
+++ user_auth/models.py
@@ -1,8 +1,9 @@
 from django.db import models
-
+from PIL import Image
 # Create your models here.
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from home.models import Product
 
 # Create your models here.
 
@@ -21,3 +22,33 @@ class User(AbstractUser):
     def __str__(self):
         return self.username
     
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    dp = models.ImageField(upload_to='images/dp/', blank=True, null=True)
+    bio = models.CharField(max_length=200, null=True, blank=True)
+    
+    def save(self, *args, **kwargs):
+        super(UserProfile, self).save(*args, **kwargs)
+
+        if self.dp:
+            img = Image.open(self.dp.path)
+            if img.width > 150 or img.height > 150:
+                output_size = (150, 150)
+                img.thumbnail(output_size)
+                img.save(self.dp.path)
+                      
+    
+class ShippingAddress(models.Model):
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)
+    full_name = models.CharField(max_length=255)
+    address_lines = models.TextField(null=True, blank=True)
+    city = models.CharField(max_length=255, null=True, blank=True)
+    state = models.CharField(max_length=255, null=True, blank=True)
+    pin_code = models.CharField(max_length=10, null=True, blank=True)
+    country = models.CharField(max_length=255, null=True, blank=True)
+    mobile = models.CharField(max_length=15)
+    status = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.full_name
+
